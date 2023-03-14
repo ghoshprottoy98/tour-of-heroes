57896627c8aea0349220bd2aa4c76fac3c0ff5af
Prottoy Ghosh <ghoshprottoy98@gmail.com>
2023-03-14 12:31:08 +0600

    master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from  '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { MessagesComponent } from './messages/messages.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroSearchComponent,
+    MessagesComponent,
+    
   ],
   imports: [
+    
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
+
+ 
+
   providers: [],
   bootstrap: [AppComponent]
 })
